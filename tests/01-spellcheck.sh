90b6a9aea18841db2da335cd0f9d3096e78396f2
Paolo Gigante <reaperzn@users.noreply.github.com>
2018-03-09 19:33:52 +0000

    Travis ci (#111)
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * testing travis-ci
    
    * Update .aspell_auter_dictionary
    
    * testing travis-ci

---
+++ tests/01-spellcheck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+r='\033[31m'
+w='\033[0m'
+g='\033[32m'
+
+EXITCODE=0
+AUTERDIR="$(cd "$(dirname "$0")" ; cd ../ ; pwd -P )"
+TESTDIR="$(cd "$(dirname "$0")" ; pwd -P )"
+echo "Testing files in ${TESTDIR}"
+
+for FILE in $(find "${AUTERDIR}" -maxdepth 1 -type f -not -path '*/\.*'); do
+  unset SPELLINGMISTAKES
+  FILE=$(realpath "${FILE}")
+  echo "Testing $FILE for spelling mistakes"
+  SPELLINGMISTAKES=$(aspell -a --personal="${TESTDIR}"/.aspell_auter_dictionary 2>&1 < "${FILE}" | egrep -iv '@|\*|Error|^$' | cut -d ' ' -f 2)
+  if [[ -n "${SPELLINGMISTAKES}" ]]; then
+    EXITCODE=1
+    SPELLINGMISTAKESCOUNT=$(echo "${SPELLINGMISTAKES}" | wc -w)
+    SPELLINGMISTAKES=$(echo "${SPELLINGMISTAKES}" | sort -u)
+    echo -e "\e[0A ${r} [ FAILED ]${w} ${SPELLINGMISTAKESCOUNT} spelling mistakes found in $FILE"
+    for WORD in ${SPELLINGMISTAKES}; do
+      echo "$WORD - $(grep -n " ${WORD}" "${FILE}" | awk -F: '{print $1}' | xargs)"
+    done
+  else
+    echo -e "\e[0A $g[ OK ] $w Tested $FILE for spelling mistakes"
+  fi
+done
+exit "${EXITCODE}"
---
+++ tests/05-shellcheck.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+r='\033[31m'
+w='\033[0m'
+g='\033[32m'
+
+EXITCODE=0
+AUTERDIR="$(cd "$(dirname "$0")" ; cd ../ ; pwd -P )"
+
+# Create a list of script files to be checked. These are files 
+ALLFILES=$(find "${AUTERDIR}" -type f -not -path '*/\.*')
+for FILE in ${ALLFILES}; do
+  grep -q '^#!/.*sh' "${FILE}" && SCRIPTSTOTEST+="${FILE} "
+done
+echo "${SCRIPTSTOTEST}"
+
+# Add non-shebang scripts manually
+SCRIPTSTOTEST+="auter.aptModule auter.yumdnfModule"
+
+# Custom shellcheck exclusions
+SHELLCHECK_EXCLUSIONS=",SC2102,SC2124,SC2155,SC2148"
+
+for SCRIPT in ${SCRIPTSTOTEST}; do
+  SCRIPT=$(realpath "${SCRIPT}")
+  SHELLCHECK_OUTPUT=$(shellcheck -e SC2044"${SHELLCHECK_EXCLUSIONS}" "${SCRIPT}")
+  if [[ $? -eq 0 ]]; then
+    echo -e "$g[ OK ] $w Tested ${SCRIPT} for common coding mistakes"
+  else
+    EXITCODE=1
+    echo -e "${r} [ FAILED ]${w} Tested ${SCRIPT} for common coding mistakes"
+    echo "${SHELLCHECK_OUTPUT}"
+  fi
+  echo "===================================="
+done
+exit "${EXITCODE}"
