39bad17b97d2906ab51ede54be097fe7548e2428
Paolo <paolo.gigante.@rackspace.co.uk>
2018-03-09 15:25:56 +0000

    testing travis-ci

--- tests/05-shellcheck.sh
+++ tests/05-shellcheck.sh
@@ -7,14 +7,24 @@ EXITCODE=0
 AUTERDIR="$(cd "$(dirname "$0")" ; cd ../ ; pwd -P )"
 TESTDIR="$(cd "$(dirname "$0")" ; pwd -P )"
 
-for FILE in $(find "${AUTERDIR}" -type f -not -path '*/\.*'); do
-  FILE=$(realpath "${FILE}")
+# Create a list of script files to be checked. These are files 
+ALLFILES=$(find "${AUTERDIR}" -type f -not -path '*/\.*')
+for FILE in ${ALLFILES}; do
+  grep -q '^#!/' "${FILE}" && SCRIPTSTOTEST+="${FILE} "
+done
+echo "${SCRIPTSTOTEST}"
+
+# Add non-shebang scripts manually
+SCRIPTSTOTEST+="auter.aptModule auter.yumdnfModule"
+
+for SCRIPT in ${SCRIPTSTOTEST}; do
+  SCRIPT=$(realpath "${SCRIPT}")
   echo "Testing $FILE for common coding mistakes"
-  SHELLCHECK_OUTPUT=$(shellcheck -e SC2044 "${FILE}")
+  SHELLCHECK_OUTPUT=$(shellcheck -e SC2044 "${SCRIPT}")
   if [[ $? -eq 0 ]]; then
-    echo -e "\e[0A $g[ OK ] $w Tested ${FILE} for common coding mistakes"
+    echo -e "\e[0A $g[ OK ] $w Tested ${SCRIPT} for common coding mistakes"
   else
-    echo -e "\e[0A ${r} [ FAILED ]${w} Tested ${FILE} for common coding mistakes"
+    echo -e "\e[0A ${r} [ FAILED ]${w} Tested ${SCRIPT} for common coding mistakes"
     echo "${SHELLCHECK_OUTPUT}"
   fi
   echo "===================================="
