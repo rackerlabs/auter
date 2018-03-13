f8ac1df081872887f6e3c62f624c416274a0ad1f
Paolo <paolo.gigante.@rackspace.co.uk>
2018-03-13 15:55:46 +0000

    Made tar less verbose

--- tests/10-rpmbuild.sh
+++ tests/10-rpmbuild.sh
@@ -5,13 +5,13 @@ for RELEASE in 6 7; do
   docker run --rm=true --name auter-rpmbuild-test-${RELEASE} -td centos:${RELEASE}
   
   # Install the rpmbuild dependencies, add the user and create the ENV
-  docker exec auter-rpmbuild-test-${RELEASE} yum -y install rpm-build elfutils-libelf rpm-libs rpm-pythoni gcc make help2man sudo
+  docker exec auter-rpmbuild-test-${RELEASE} yum -y -q install rpm-build elfutils-libelf rpm-libs rpm-pythoni gcc make help2man sudo
   docker exec auter-rpmbuild-test-${RELEASE} useradd builduser
   docker exec auter-rpmbuild-test-${RELEASE} mkdir -p /home/builduser/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
   echo '%_topdir %(echo $HOME)/rpmbuild' > /tmp.rpmmacros
   
   # Create the tarball for rpmbuild
-  tar -czvf ../auter-"$(grep Version auter.spec | awk '{print $2}')"-rpmbuild.tar.gz ../auter
+  tar -czf ../auter-"$(grep Version auter.spec | awk '{print $2}')"-rpmbuild.tar.gz ../auter
   
   # Copy the rpmbuild config and tarball to the builduser homedir
   docker cp /tmp/.rpmmacros auter-rpmbuild-test-${RELEASE}:/home/builduser/.rpmmacros
--- tests/11-container-rpmbuild.sh
+++ tests/11-container-rpmbuild.sh
@@ -6,12 +6,12 @@ if [[ $(whoami) == "root" ]]; then
 else
    echo "I am builduser... I am building"
    cd /home/builduser
-   tar -xzvf /home/builduser/*.tar.gz
+   tar -xzf /home/builduser/*.tar.gz
    mv /home/builduser/auter /home/builduser/auter-"$(grep Version auter/auter.spec | awk '{print $2}')"
-   tar -cvzf "$(grep Version auter*/auter.spec | awk '{print $2}')".tar.gz auter-"$(grep Version auter*/auter.spec | awk '{print $2}')"
+   tar -czf "$(grep Version auter*/auter.spec | awk '{print $2}')".tar.gz auter-"$(grep Version auter*/auter.spec | awk '{print $2}')"
    mv "$(grep Version auter*/auter.spec | awk '{print $2}')".tar.gz rpmbuild/SOURCES
    cd /home/builduser/rpmbuild/SPECS
    rpmbuild -ba auter.spec
    cd /home/builduser/rpmbuild/RPMS/noarch
-   tar -cvzf /home/builduser/auter.rpm.tar.gz auter*rpm
+   tar -czf /home/builduser/auter.rpm.tar.gz auter*rpm
 fi
