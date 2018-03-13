df61f6384dca1fcab91951fc4643e00657cee176
Paolo Gigante <reaperzn@users.noreply.github.com>
2018-03-13 15:36:50 +0000

    Good travis (#118)
    * Better travis tests which only checke the current changes
    
    * Cleaned up output of 01-spellcheck.sh
    
    * Added comments to .travis.yml
    
    * made yum quieter
    
    * adjusted method of pulling upstream master

--- tests/01-spellcheck.sh
+++ tests/01-spellcheck.sh
@@ -8,10 +8,10 @@ g='\033[32m'
 EXITCODE=0
 AUTERDIR="$(cd "$(dirname "$0")" ; cd ../ ; pwd -P )"
 TESTDIR="$(cd "$(dirname "$0")" ; pwd -P )"
+CHANGEDFILES="$(find "${AUTERDIR}" -name CHANGEDFILES)"
 
-echo "Testing files in ${TESTDIR}"
-if [[ -f "${AUTERDIR}"/CHANGEDFILES ]]; then
-  FILELIST=$(find "${AUTERDIR}" -type f -not -path '*/\.*' | egrep "$(cat CHANGEDFILES | xargs | tr ' ' '|')")
+if [[ -f "${CHANGEDFILES}" ]]; then
+  FILELIST=$(find "${AUTERDIR}" -type f -not -path '*/\.*' | egrep "$(xargs <"${CHANGEDFILES}" | tr ' ' '|')")
 else
   FILELIST=$(find "${AUTERDIR}" -type f -not -path '*/\.*')
 fi
@@ -19,18 +19,17 @@ fi
 for FILE in ${FILELIST}; do 
   unset SPELLINGMISTAKES
   FILE=$(realpath "${FILE}")
-  echo "Testing $FILE for spelling mistakes"
   SPELLINGMISTAKES=$(aspell -a --personal="${TESTDIR}"/.aspell_auter_dictionary 2>&1 < "${FILE}" | egrep -iv '@|\*|Error|^$' | cut -d ' ' -f 2)
   if [[ -n "${SPELLINGMISTAKES}" ]]; then
     EXITCODE=1
     SPELLINGMISTAKESCOUNT=$(echo "${SPELLINGMISTAKES}" | wc -w)
     SPELLINGMISTAKES=$(echo "${SPELLINGMISTAKES}" | sort -u)
-    echo -e "\e[0A ${r} [ FAILED ]${w} ${SPELLINGMISTAKESCOUNT} spelling mistakes found in $FILE"
+    echo -e "${r} [ FAILED ]${w} ${SPELLINGMISTAKESCOUNT} spelling mistakes found in $FILE"
     for WORD in ${SPELLINGMISTAKES}; do
       echo "$WORD - $(grep -n " ${WORD}" "${FILE}" | awk -F: '{print $1}' | xargs)"
     done
   else
-    echo -e "\e[0A $g[ OK ] $w Tested $FILE for spelling mistakes"
+    echo -e "$g[ OK ] $w Tested $FILE for spelling mistakes"
   fi
 done
 exit "${EXITCODE}"
