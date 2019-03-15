7962bb1faac51e6f0cf0f5f7df14a2a1decef915
Nick Rhodes <nrhodes91@gmail.com>
2019-03-15 06:32:29 +0000

    Simplify 01-spellcheck.sh

--- tests/01-spellcheck.sh
+++ tests/01-spellcheck.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
+
 r='\033[31m'
 w='\033[0m'
 g='\033[32m'
 
-[[ ! -f /usr/bin/aspell ]] && echo "apsell not installed. Aborting test" && exit 1
+if ! command -v aspell &>/devnull; then echo "apsell not installed. Aborting test"; exit 1; fi
 
 EXITCODE=0
-AUTERDIR="$(cd "$(dirname "$0")" ; cd ../ ; pwd -P )"
-TESTDIR="$(cd "$(dirname "$0")" ; pwd -P )"
+AUTERDIR="$(dirname `pwd`)"
+TESTDIR="$(pwd)"
 CHANGEDFILES="$(find "${AUTERDIR}" -name CHANGEDFILES)"
 
 if [[ -f "${CHANGEDFILES}" ]]; then
-  FILELIST=$(find "${AUTERDIR}" -type f -not -path '*/\.*' | egrep "$(xargs <"${CHANGEDFILES}" | tr ' ' '|')")
+  readarray -t FILELIST <<< "$(find "${AUTERDIR}" -type f -not -path '*/\.*' | egrep "$(xargs <"${CHANGEDFILES}" | tr ' ' '|')")"
 else
-  FILELIST=$(find "${AUTERDIR}" -type f -not -path '*/\.*')
+  readarray -t FILELIST < <(find "${AUTERDIR}" -type f -not -path '*/\.*')
 fi
 
-for FILE in ${FILELIST}; do 
-  unset SPELLINGMISTAKES
+for FILE in "${FILELIST[@]}"; do
   FILE=$(realpath "${FILE}")
   SPELLINGMISTAKES=$(aspell -a --personal="${TESTDIR}"/.aspell_auter_dictionary 2>&1 < "${FILE}" | egrep -iv '@|\*|Error|^$' | cut -d ' ' -f 2)
   if [[ -n "${SPELLINGMISTAKES}" ]]; then
     EXITCODE=1
     SPELLINGMISTAKESCOUNT=$(echo "${SPELLINGMISTAKES}" | wc -w)
     SPELLINGMISTAKES=$(echo "${SPELLINGMISTAKES}" | sort -u)
-    echo -e "${r} [ FAILED ]${w} ${SPELLINGMISTAKESCOUNT} spelling mistakes found in $FILE"
+    echo -e "${r}[ FAILED ]${w} ${SPELLINGMISTAKESCOUNT} spelling mistakes found in $FILE"
     for WORD in ${SPELLINGMISTAKES}; do
       echo "$WORD - $(grep -n " ${WORD}" "${FILE}" | awk -F: '{print $1}' | xargs)"
     done
